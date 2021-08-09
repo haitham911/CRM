53506a5aab78eb9744d662a25a5793aaa5d02114
haitham911 <haitham911eg@gmail.com>
2021-08-10 00:13:48 +0200

    updates

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,5 +1,6 @@
 export default {
-  api: 'http://127.0.0.1:8000/api', /* type http://crm-api.id-a.pl/api if you want to use demo API http://127.0.0.1:8000/api */
+  api:
+    'https://crmbackend.compareha.com/api' /* type http://crm-api.id-a.pl/api if you want to use demo API http://127.0.0.1:8000/api */,
   prefix: 'auth',
   paths: {
     login: 'login',
